1d0333f9ab52933b8f593b8d0b83c14acfc0fbc4
Ronald Zenón Cuevas Machaca <rzcuevasm@gmail.com>
2024-10-16 20:29:02 -0500

    modifica formulario

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            checkBox1 = new CheckBox();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -40,19 +44,53 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(52, 74);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(57, 152);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(8, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(57, 227);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(137, 104);
+            dataGridView1.TabIndex = 3;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox1);
+            Controls.Add(checkBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private CheckBox checkBox1;
+        private ComboBox comboBox1;
+        private DataGridView dataGridView1;
     }
 }
